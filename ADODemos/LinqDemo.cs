ad76b7644cd655939a451a896b00cd32e8d1fe3a
Anamika Sawhney <anamika.sawhney22@gmail.com>
2022-04-06 15:27:12 +0530

    ADO & Liq Demos:

---
+++ ADODemos/LinqDemo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADODemos
+{
+    class LinqDemo
+    {
+        static void Main()
+        {
+            int[] num = new int[] { 1, 2, 3, 4, 5, 6 };
+            // LINQ
+            var list = (from x in num
+                        select x);
+            foreach(var temp in list)
+                Console.WriteLine(temp);
+
+            var sum = (from x in num
+                       select x).Sum();
+
+            Console.WriteLine("Sum is " + sum);
+
+            var avg = (from x in num
+                       select x).Average();
+            Console.WriteLine("Avg is  " + avg);
+
+            var evenlist = (from x in num
+                        where x % 2 == 0
+                        select x);
+
+            Console.WriteLine("Even Nums");
+            foreach(var temp in evenlist)
+                Console.WriteLine(temp);
+
+            List<string> names = new List<string>()
+            {
+                 "Ajay Kumar", "Deepak Sahni", "Arun Kumar" };
+
+            var nameslist = (from x in names
+
+                             select x);
+
+            foreach(string temp in nameslist)
+                Console.WriteLine(temp);
+
+            var kumarlist = (from x in names
+                             where x.Contains("Kumar")
+                             select x);
+            Console.WriteLine("List of kumars");
+            foreach (string temp in kumarlist)
+                Console.WriteLine(temp);
+
+        }
+    }
+}
---
+++ ADODemos/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//  Step 1
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ADODemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice = "y";
+            while (choice == "y")
+            {
+                int ch= Menu();
+
+                switch(ch)
+                {
+                    case 1 : GetEmployees(); break;
+                    case 2:
+                        {
+                            Console.WriteLine("Enter Name");
+                            string name = Console.ReadLine();
+                            Console.WriteLine("Enter Manager Name");
+                            string managername = Console.ReadLine();
+                            InsertRecord(name, managername); break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Enter Id");
+                            int id = Byte.Parse(Console.ReadLine());
+                            
+                             DeleteEmployee(id);break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Enter Id");
+                            int id = Byte.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter Manager Name");
+                            string managername = Console.ReadLine();
+                            EditEmployee(id, managername); break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("Enter Id");
+                            int id = Byte.Parse(Console.ReadLine());
+                            SearchDepartment(id);
+                            break;
+                        }
+                }
+                //Console.WriteLine("Enter Id");
+                //int id = Byte.Parse(Console.ReadLine());
+               
+              
+                Console.WriteLine("Do you want to repeat");
+                choice = Console.ReadLine();
+            }
+ 
+        }
+
+        private static int Menu()
+        {
+            Console.WriteLine("Main Menu");
+            Console.WriteLine("1. List of Departments");
+            Console.WriteLine("2. Insert Department Record");
+            Console.WriteLine("3. Delete Department");
+            Console.WriteLine("4. Edit Department");
+            Console.WriteLine("5. Search Department");
+            Console.WriteLine("Enter Choice");
+            int ch = Byte.Parse(Console.ReadLine());
+            return ch;
+        }
+
+        private static string getConnectionString()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Myconnection"].ToString();
+            return connectionString;
+        }
+        private static SqlConnection GetConnection()
+        {
+           //string connectionString = "data source=LAPTOP-53S2KQS8;initial catalog=practicedb;integrated security=true";
+            SqlConnection connection = new SqlConnection(getConnectionString());
+            return connection;
+
+
+        }
+        private static void GetEmployees()
+        {
+
+            // Step 2
+             using (SqlConnection connection = GetConnection() )
+            {
+                // Step 3
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.CommandText = "Select * from department";
+                    command.Connection = connection;
+
+                    //SqlCommand com = new SqlCommand("Select * from Employee", connection);
+
+                    // Step 4:
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader["id"].ToString() + " " + reader["name"] + " " + reader["managername"]);
+                        }
+                    }
+                    reader.Close();
+                    connection.Close();
+                }
+            }
+        }
+        static void InsertRecord(string name, string managername)
+        {
+            SqlConnection connection = GetConnection();
+            SqlCommand command = new SqlCommand("Insert into Department (name,managername) values(@name, @managername)", connection);
+            //command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@managername", managername);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            command.Dispose();
+            connection.Dispose();
+
+
+
+        }
+        static void SearchDepartment(int id)
+        {
+            using (SqlConnection connection = GetConnection())
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.CommandText = "Select * From Department where id=@id";
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Connection = connection;
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+
+                        Console.WriteLine(reader["id"].ToString() + " " + reader["name"] + " " + reader["managername"]);
+
+                    }
+                    reader.Close();
+                    connection.Close();
+                }
+            }
+
+        }
+                static void DeleteEmployee(int id)
+        {
+           using (SqlConnection connection = GetConnection())
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.CommandText = "Delete from department where id=@id";
+                    command.Connection = connection;
+                    command.Parameters.AddWithValue("@id", id);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+
+
+        static void EditEmployee(int id, string managername)
+        {
+            using (SqlConnection connection = GetConnection())
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.CommandText = "update department set managername=@managername where id=@id";
+                    command.Connection = connection;
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@managername", managername);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
